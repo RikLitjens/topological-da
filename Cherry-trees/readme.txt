10c97023e68150ebe45b43bbea1939d42e7e6a2f
Olivier Boeren <olivierboeren@live.nl>
2023-07-05 22:26:12 +0200

    add: arg for strategy

--- Cherry-trees/readme.txt
+++
@@ -1,3 +0,0 @@
-To use the cherry_trees dataset, make sure you add the absolute path to your dataset in the config.ini file! 
-From there, it will automatically find your datasets, granted that the structure of bags is unchanged. 
-
--- Cherry-trees/src/main.py
+++ Cherry-trees/src/main.py
@@ -14,43 +14,32 @@ from popsearch.mst import *
 from preprocess import clean_up, rotate_z_up
 import pickle
 from strategies import *
+import argparse
+
+# Create the argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--method', type=str)
 
 
-# Strategy to use
-strat = "CNN"
-# strat = "homology"
-# strat = "reeb"
+if __name__ == '__main__':
+    args = parser.parse_args()
+    
+    local_path = get_data_path()
+    bag_id = 0
 
-#############################
-# Point cloud preprocessing #
-#############################
+    # Load the point cloud
+    pcd = load_point_cloud(local_path, bag_id, "cloud_final")
+    pcd = rotate_z_up(pcd)
+    pcd = clean_up(pcd)
 
-local_path = get_data_path()
-bag_id = 0
+    match args.method:
+        case "cnn":
+            strat_CNN(pcd, prepped_model=True, bag_id=bag_id)
+        case "homology":
+            strat_persistent_homology(pcd)
+        case "reeb":
+            strat_reeb_graph(pcd)
+        case _:
+            raise(f"{args.method} is an unknown method. valid values are: 'cnn', 'homology' and 'reeb'")
 
-# Load the point cloud
-pcd = load_point_cloud(local_path, bag_id, "cloud_final")
-pcd = rotate_z_up(pcd)
-pcd = clean_up(pcd)
-
-################
-# CNN strategy #
-################
-
-if strat == "CNN":
-    strat_CNN(pcd, prepped_model=True, bag_id=bag_id)
-
-#############################
-# Persistent homology strat #
-#############################
-
-if strat == "homology":
-    strat_persistent_homology(pcd)
-
-#######################
-# Reeb graph strategy #
-#######################
-
-if strat == "reeb":
-    strat_reeb_graph(pcd)
 
